5a0eb8ce132102336b246df4d3a19556ca93b3e4
liyiliang <lyl120751@alibaba-inc.com>
2018-03-12 15:18:24 +0800

    同步

--- cs-client/src/main/java/com/cornerstone/CsClientApplication.java
+++ cs-client/src/main/java/com/cornerstone/CsClientApplication.java
@@ -1,7 +1,11 @@
 package com.cornerstone;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author: liyl
@@ -9,10 +13,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0.0
  */
 @SpringBootApplication
+@EnableCaching
+@RestController
 public class CsClientApplication {
+
+    @Autowired
+    private RedisClient redisClient;
 
     public static void main(String[] args) {
         SpringApplication.run(CsClientApplication.class, args);
 
     }
+
+
+
+    @RequestMapping("/set")
+    public String set(String key, String value) throws Exception{
+        redisClient.set(key, value);
+        return "success";
+    }
+
+    @RequestMapping("/get")
+    public String get(String key) throws Exception {
+        return redisClient.get(key);
+    }
+
+
 }
