a447bc9ca3f5068429364f2d1afe2c2dfc2a4d71
liyiliang <li.yiliang@yiruikecorp.com>
2018-09-29 15:28:14 +0800

    先能跑起来

---
+++ cs-console/src/main/java/com/cs/console/CsConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.cs.console;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsConsoleApplication.class, args);
+    }
+}
---
+++ cs-console/src/main/java/com/cs/console/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.cs.console.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerSpringMvcPlugin(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("CornerStone接口列表")
+                .description("简单优雅的restfun风格")
+                .contact(new Contact("firnice", "https://github.com/firnice", "firnice@hotmail.com"))//作者
+                .version("1.0")
+                .build();
+
+    }
+
+}
