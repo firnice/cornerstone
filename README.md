e36c8070cf03b7cdad15eea30abacd0ff719f52e
liyiliang <li.yiliang@yiruikecorp.com>
2018-10-11 10:57:50 +0800

    增加thymeleaf

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 技术集合，个人兴趣
 
 ## 技术选型
-- 核心框架 :Spring Boot
+框架 :
+ - Spring Boot
+ - thymeleaf
+ 
+## 功能
+- 权限管理系统
+- 菜单
 
  
 
--- cs-console/src/main/java/com/cs/console/interfaces/controller/HomeController.java
+++ cs-console/src/main/java/com/cs/console/interfaces/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.cs.console.interfaces.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping
-    public String home(){
-        return "hello world";
+    public String home(Model model){
+        model.addAttribute("name","world");
+        return "home";
     }
 }
