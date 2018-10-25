98f6e2e6c3816b512506504970e7b05264ec0784
liyiliang <li.yiliang@yiruikecorp.com>
2018-10-25 18:28:49 +0800

    视频压缩优化

--- cs-console/src/test/java/com/cs/console/service/VideoServiceTest.java
+++ cs-console/src/test/java/com/cs/console/service/VideoServiceTest.java
@@ -17,25 +17,27 @@ public class VideoServiceTest {
         FFprobe ffprobe = new FFprobe("/usr/local/bin/ffprobe");
 
 
-        String inFilename = "/Users/yiruike/Downloads/6.mp4";
+        String inFilename = "/Users/yiruike/Downloads/9.mp4";
         FFmpegProbeResult in = ffprobe.probe(inFilename);
 
         FFmpegBuilder builder =
                 new FFmpegBuilder()
-                        .setInput(inFilename) // Filename, or a FFmpegProbeResult
+//                        .setInput(inFilename) // Filename, or a FFmpegProbeResult
                         .setInput(in)
                         .overrideOutputFiles(true) // Override the output if it exists
                         .addOutput("output.mp4") // Filename for the destination
                         .setFormat("mp4") // Format is inferred from filename, or can be set
-                        .setTargetSize(1024_000) // Aim for a 250KB file
+//                        .setTargetSize(1024_000) // Aim for a 250KB file
+                        .setVideoBitRate(1000000)
                         .disableSubtitle() // No subtiles
                         .setAudioChannels(1) // Mono audio
                         .setAudioCodec("aac") // using the aac codec
                         .setAudioSampleRate(48_000) // at 48KHz
                         .setAudioBitRate(32768) // at 32 kbit/s
                         .setVideoCodec("libx264") // Video using x264
-                        .setVideoFrameRate(24, 1) // at 24 frames per second
-                        .setVideoResolution(1280, 720) // at 640x480 resolution
+                        .setVideoFrameRate(30) // at 24 frames per second
+//                        .setVideoResolution(1280, 720) // at 640x480 resolution
+                        .setVideoFilter("scale=-1:480")
                         .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
                         .done();
 
